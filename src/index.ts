658b761ab96398de62810f0d83bc948e3b4a2baf
Matt Mazzola <mattmazzola@live.com>
2020-09-19 17:40:44 -0700

    Attempt to use reloadEnv option

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,6 @@ console.log(`DEBUG=${process.env.DEBUG}`)
 
 const debugInfo = debug('info')
 debugInfo('Expect this to be logged')
+
+const debugInfoReload = (debug as any)('reload', { reloadEnv: true })
+debugInfoReload('Reloaded environment variables. Expect this to be logged')
